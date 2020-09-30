1d90a7c73c5059eecb91f7af6b7df48c58cc0758
Veronika Romashkina <vrom911@gmail.com>
2020-09-30 16:32:14 +0100

    Add tests for Chapter 1

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,16 @@
 module Main (main) where
 
+import Test.Hspec (hspec)
+
+import Test.Chapter1
+import Test.Chapter2
+import Test.Chapter3
+import Test.Chapter4
+
 
 main :: IO ()
-main = putStrLn "Tests are not implemented"
+main = hspec $ do
+    chapter1
+    chapter2
+    chapter3
+    chapter4
---
+++ test/Test/Chapter3.hs
@@ -0,0 +1,13 @@
+module Test.Chapter3
+    ( chapter3
+    ) where
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+import Chapter3
+
+
+chapter3 :: Spec
+chapter3 = describe "Chapter3" $ do
+    describe "Chapter3Normal" $ it "" $ True `shouldBe` True
+    describe "Chapter3Advanced" $ it "" $ True `shouldBe` True
