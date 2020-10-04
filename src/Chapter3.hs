f165acbcb14098368e93e6212fc13b1e882c060f
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2020-10-04 20:26:04 +0300

    add book data class

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -343,6 +343,13 @@ Define the Book product data type. You can take inspiration from our description
 of a book, but you are not limited only by the book properties we described.
 Create your own book type of your dreams!
 -}
+data Book = MkBook
+    { bookAuthor :: String
+    , bookTitle  :: String
+    , bookYear   :: Int
+    , bookPages  :: Int
+    , bookPrice  :: Int
+    } deriving (Show)
 
 {- |
 =⚔️= Task 2
