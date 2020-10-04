457020e1f86efab66ff5077ac6d1423fe610c16d
Sayan <7rat13@gmail.com>
2020-10-04 22:52:44 +0530

    Solve Chapter1

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -209,31 +209,31 @@ So, the output in this example means that 'False' has type 'Bool'.
 > Try to guess first and then compare your expectations with GHCi output
 
 >>> :t True
-<INSERT THE RESULT INSTEAD OF THE TEXT>
+True :: Bool
 >>> :t 'a'
-<INSERT THE RESULT INSTEAD OF THE TEXT>
+'a' :: Char
 >>> :t 42
-<INSERT THE RESULT INSTEAD OF THE TEXT>
+42 :: Num p => p
 
 A pair of boolean and char:
 >>> :t (True, 'x')
-<INSERT THE RESULT INSTEAD OF THE TEXT>
+(True, 'x') :: (Bool, Char)
 
 Boolean negation:
 >>> :t not
-<INSERT THE RESULT INSTEAD OF THE TEXT>
+not :: Bool -> Bool
 
 Boolean 'and' operator:
 >>> :t (&&)
-<INSERT THE RESULT INSTEAD OF THE TEXT>
+(&&) :: Bool -> Bool -> Bool
 
 Addition of two numbers:
 >>> :t (+)
-<INSERT THE RESULT INSTEAD OF THE TEXT>
+(+) :: Num a => a -> a -> a
 
 Maximum of two values:
 >>> :t max
-<INSERT THE RESULT INSTEAD OF THE TEXT>
+max :: Ord a => a -> a -> a
 
 You might not understand each type at this moment, but don't worry! You've only
 started your Haskell journey. Types will become your friends soon.
@@ -301,30 +301,42 @@ expressions in GHCi
   functions and operators first. Remember this from the previous task? ;)
 
 >>> 1 + 2
-INSERT THE RESULT INSTEAD OF THE TEXT
+3
 >>> 10 - 15
+-5
 
 >>> 10 - (-5)  -- negative constants require ()
+15
 
 >>> (3 + 5) < 10
+True
 
 >>> True && False
+False
 
 >>> 10 < 20 || 20 < 5
+True
 
 >>> 2 ^ 10  -- power
+1024
 
 >>> not False
+True
 
 >>> div 20 3  -- integral division
+6
 
 >>> mod 20 3  -- integral division remainder
+2
 
 >>> max 4 10
+10
 
 >>> min 5 (max 1 2)
+2
 
 >>> max (min 1 10) (min 5 7)
+5
 
 
 Because Haskell is a __statically-typed__ language, you see an error each time
@@ -417,6 +429,7 @@ task is to specify the type of this function.
 49
 -}
 
+squareSum :: Int -> Int -> Int
 squareSum x y = (x + y) * (x + y)
 
 
@@ -437,7 +450,7 @@ Implement the function that takes an integer value and returns the next 'Int'.
   function body with the proper implementation.
 -}
 next :: Int -> Int
-next x = error "next: not implemented!"
+next x = x + 1
 
 {- |
 After you've implemented the function (or even during the implementation), you
@@ -478,7 +491,9 @@ Implement a function that returns the last digit of a given number.
   whether it works for you!
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+
+lastDigit :: Int -> Int
+lastDigit n = n `mod` 10
 
 
 {- |
@@ -508,7 +523,11 @@ branches because it is an expression and it must always return some value.
   satisfying the check will be returned and, therefore, evaluated.
 -}
 closestToZero :: Int -> Int -> Int
-closestToZero x y = error "closestToZero: not implemented!"
+closestToZero x y =
+  if abs x < abs y
+    then x
+    else y
+
 
 
 {- |
@@ -542,7 +561,11 @@ value after "=" where the condition is true.
 Casual reminder about adding top-level type signatures for all functions :)
 -}
 
-mid x y z = error "mid: not implemented!"
+mid :: Int -> Int -> Int -> Int
+mid x y z
+  | x < y && y < z || z < y && y < x = y
+  | y < x && x < z || z < x && x < z = x
+  | otherwise = z
 
 {- |
 =⚔️= Task 8
@@ -556,7 +579,10 @@ True
 >>> isVowel 'x'
 False
 -}
-isVowel c = error "isVowel: not implemented!"
+isVowel :: Char -> Bool
+isVowel c
+  | elem c "aeiou" = True
+  | otherwise = False
 
 
 {- |
@@ -620,7 +646,11 @@ Try to introduce variables in this task (either with let-in or where) to avoid
 specifying complex expressions.
 -}
 
-sumLast2 n = error "sumLast2: Not implemented!"
+sumLast2 :: Int -> Int
+sumLast2 n = theLastDigit + secondLastDigit
+  where
+    theLastDigit = n `mod` 10
+    secondLastDigit = (n `mod` 100 - theLastDigit) `div` 10
 
 
 {- |
@@ -641,7 +671,10 @@ You need to use recursion in this task. Feel free to return to it later, if you
 aren't ready for this boss yet!
 -}
 
-firstDigit n = error "firstDigit: Not implemented!"
+firstDigit :: Int -> Int
+firstDigit n
+  | abs (n `div` 10) <= 1 = n
+  | otherwise = firstDigit ((n - n `mod` 10) `div` 10)
 
 
 {-
@@ -655,4 +688,5 @@ and summon @vrom911 and @chshersh for the review!
 Modules: http://learnyouahaskell.com/modules
 Let vs where: https://wiki.haskell.org/Let_vs._Where
 Packages and modules in Haskell: https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/packages.html
+
 -}
