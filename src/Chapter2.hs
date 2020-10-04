3ff312f57a0eb8f67f34a0bba9676e9cc01ffac6
Yu-zh <yu.zhang.yz862@yale.edu>
2020-10-04 17:23:18 -0400

    Update src/Chapter2.hs
    Co-authored-by: hint-man[bot] <44720633+hint-man[bot]@users.noreply.github.com>

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -625,7 +625,7 @@ Implement a function that duplicates each element of the list
 -}
 duplicate :: [a] -> [a]
 duplicate [] = []
-duplicate (x:xs) = (x : x : duplicate xs)
+duplicate (x:xs) = x : x : duplicate xs
 
 
 {- |
