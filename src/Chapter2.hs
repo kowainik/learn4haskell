aacd4cab0e83f0378ea71d04b5be992aa042998b
Adria M. Hadi <adriahadi@tuta.io>
2020-10-06 16:16:13 +0700

    Chapter 2 Solution

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -47,7 +47,7 @@ Before we start diving into the FP concepts, let's cover an important
 process-development detail.
 
 Without any additional effort, in Haskell you have access to all
-functions defined in the special module called "Prelude". This module
+functions defined in the special module called "Prelude". This module3
 is always imported by default and contains all primitive data types
 and useful functions to work with them. However, sometimes you need to
 import other modules even from other libraries.
@@ -136,34 +136,43 @@ functions in GHCi and insert the corresponding resulting output below:
 
 List of booleans:
 >>> :t [True, False]
+[True, False] :: [Bool]
 
 String is a list of characters:
 >>> :t "some string"
+"some string" :: [Char]
 
 Empty list:
 >>> :t []
+[] :: [a]
 
 Append two lists:
 >>> :t (++)
+(++) :: [a] -> [a] -> [a]
 
 Prepend an element at the beginning of a list:
 >>> :t (:)
+(:) :: a -> [a] -> [a]
 
 Reverse a list:
 >>> :t reverse
+reverse :: [a] -> [a]
 
 Take first N elements of a list:
 >>> :t take
+take :: Int -> [a] -> [a]
 
 Create list from N same elements:
 >>> :t replicate
+replicate :: Int -> a -> [a]
 
 Split a string by line breaks:
 >>> :t lines
+lines :: String -> [String]
 
 Join a list of strings with line breaks:
 >>> :t unlines
-
+unlines :: [String] -> String
 
 -}
 
@@ -177,31 +186,43 @@ Evaluate the following expressions in GHCi and insert the answers. Try
 to guess first, what you will see.
 
 >>> [10, 2] ++ [3, 1, 5]
+[10,2,3,1,5]
 
 >>> [] ++ [1, 4]  -- [] is an empty list
+[1,4]
 
 >>> 3 : [1, 2]
+[3,1,2]
 
 >>> 4 : 2 : [5, 10]  -- prepend multiple elements
+[4,2,5,10]
 
 >>> [1 .. 10]  -- list ranges
+[1,2,3,4,5,6,7,8,9,10]
 
 >>> [10 .. 1]
+[]
 
 >>> [10, 9 .. 1]  -- backwards list with explicit step
+[10,9,8,7,6,5,4,3,2,1]
 
 >>> length [4, 10, 5]  -- list length
+3
 
 >>> replicate 5 True
+[True,True,True,True,True]
 
 >>> take 5 "Hello, World!"
+"Hello"
 
 >>> drop 5 "Hello, World!"
+", World!"
 
 >>> zip "abc" [1, 2, 3]  -- convert two lists to a single list of pairs
+[('a',1),('b',2),('c',3)]
 
 >>> words "Hello   Haskell     World!"  -- split the string into the list of words
-
+["Hello","Haskell","World!"]
 
 
 👩‍🔬 Haskell has a lot of syntax sugar. In the case with lists, any
@@ -327,8 +348,13 @@ from it!
 ghci> :l src/Chapter2.hs
 -}
 subList :: Int -> Int -> [a] -> [a]
-subList = error "subList: Not implemented!"
-
+subList x y alist 
+  | length alist == 0 = []
+  | x < 0 || y < 0 = []
+  | otherwise =
+      let
+        end = y + 1
+      in drop x (take end alist)
 {- |
 =⚔️= Task 4
 
@@ -339,8 +365,12 @@ Implement a function that returns only the first half of a given list.
 >>> firstHalf "bca"
 "b"
 -}
--- PUT THE FUNCTION TYPE IN HERE
-firstHalf l = error "firstHalf: Not implemented!"
+
+firstHalf :: [a] -> [a]
+firstHalf aList =
+  let
+    listLength = length aList
+  in take (div listLength 2) aList
 
 
 {- |
@@ -491,7 +521,9 @@ True
 >>> isThird42 [42, 42, 0, 42]
 False
 -}
-isThird42 = error "isThird42: Not implemented!"
+isThird42 :: [Int] -> Bool
+isThird42 (_ : _ : 42: _) = True
+isThird42 _ = False
 
 
 {- |
@@ -596,7 +628,8 @@ Implement a function that duplicates each element of the list
 
 -}
 duplicate :: [a] -> [a]
-duplicate = error "duplicate: Not implemented!"
+duplicate [] = []
+duplicate (x:xs) = x : x : duplicate xs
 
 
 {- |
@@ -611,7 +644,10 @@ Write a function that takes elements of a list only on even positions.
 >>> takeEven [2, 1, 3, 5, 4]
 [2,3,4]
 -}
-takeEven = error "takeEven: Not implemented!"
+takeEven :: [a] -> [a]
+takeEven [] = []
+takeEven (x : []) = [x]
+takeEven (x : _: xs) = x : takeEven xs
 
 {- |
 =🛡= Higher-order functions
@@ -718,7 +754,7 @@ value of the element itself
 🕯 HINT: Use combination of 'map' and 'replicate'
 -}
 smartReplicate :: [Int] -> [Int]
-smartReplicate l = error "smartReplicate: Not implemented!"
+smartReplicate aList = concat (map (\x -> replicate x x) aList)
 
 {- |
 =⚔️= Task 9
@@ -731,7 +767,8 @@ the list with only those lists that contain a passed element.
 
 🕯 HINT: Use the 'elem' function to check whether an element belongs to a list
 -}
-contains = error "contains: Not implemented!"
+contains :: Int -> [[Int]] -> [[Int]]
+contains n aList = filter (elem n) aList
 
 
 {- |
@@ -771,13 +808,17 @@ Let's now try to eta-reduce some of the functions and ensure that we
 mastered the skill of eta-reducing.
 -}
 divideTenBy :: Int -> Int
-divideTenBy x = div 10 x
+divideTenBy = div 10
 
 -- TODO: type ;)
-listElementsLessThan x l = filter (< x) l
+--listElementsLessThan x l= filter (< x) l
+listElementsLessThan :: Int -> [Int] -> [Int]
+listElementsLessThan x = filter (< x) 
 
 -- Can you eta-reduce this one???
-pairMul xs ys = zipWith (*) xs ys
+--pairMul xs ys = zipWith (*) xs ys
+pairMul :: [Int] -> [Int] -> [Int]
+pairMul = zipWith (*) 
 
 {- |
 =🛡= Lazy evaluation
@@ -832,7 +873,16 @@ list.
 
 🕯 HINT: Use the 'cycle' function
 -}
-rotate = error "rotate: Not implemented!"
+rotate :: Int -> [a] -> [a]
+rotate n aList
+  | n < 0 || length aList == 0 = []
+  | otherwise =
+      take listLength (drop n (take repeatCycle (cycle aList)))
+      where
+        listLength = length aList
+        repeatCycle = listLength * (n + 1)
+
+        
 
 {- |
 =💣= Task 12*
@@ -848,7 +898,9 @@ and reverses it.
   function, but in this task, you need to implement it manually. No
   cheating!
 -}
-rewind = error "rewind: Not Implemented!"
+rewind :: [a] -> [a]
+rewind [] = []
+rewind (x:xs) = rewind xs ++ [x]
 
 
 {-
