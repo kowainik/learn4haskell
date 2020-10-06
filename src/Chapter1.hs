c7cb40f91ebd9b4ddca60065c408c9d23af507ba
onlyasmalllizard <30602046+onlyasmalllizard@users.noreply.github.com>
2020-10-06 16:34:59 +0100

    Update src/Chapter1.hs
    Co-authored-by: hint-man[bot] <44720633+hint-man[bot]@users.noreply.github.com>

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -549,7 +549,7 @@ Casual reminder about adding top-level type signatures for all functions :)
   -}
 
 mid :: Int -> Int -> Int -> Int
-mid x y z 
+mid x y z
   | (x < y && x > z) || (x > y && x < z) = x
   | (y < x && y > z) || (y > x && y < z) = y
   | (z < y && z > x) || (z > y && z < x) = z
