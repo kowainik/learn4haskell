345e75abfd0d29a83a653de02fb471794b503dd2
kopackiw <wojciech.kopacki@finder.com>
2020-10-14 22:16:21 +0200

    feat: finish ex 5

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -478,8 +478,8 @@ Implement a function that returns the last digit of a given number.
   results. Or you can try to guess the function name, search for it and check
   whether it works for you!
 -}
--- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Integer -> Integer
+lastDigit n = mod n 10
 
 
 {- |
