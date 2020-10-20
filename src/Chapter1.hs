a0725d2f18d99ef16affbd1cc7a51d1b7ae8273d
David Ruwadi <druwadi13@gmail.com>
2020-10-20 01:50:11 -0400

    spacing

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -481,8 +481,7 @@ Implement a function that returns the last digit of a given number.
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
 lastDigit :: Int -> Int
 lastDigit n = mod (abs n) 10
-
-
+ 
 {- |
 =⚔️= Task 6
 
