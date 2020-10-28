e07b0ca5e7d5c5c35f986393fab0892174029a48
sxxrfx <52241057+sxxrfx@users.noreply.github.com>
2020-10-28 19:50:33 +0530

    Update src/Chapter2.hs
    Co-authored-by: hint-man[bot] <44720633+hint-man[bot]@users.noreply.github.com>

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -747,7 +747,7 @@ the list with only those lists that contain a passed element.
 🕯 HINT: Use the 'elem' function to check whether an element belongs to a list
 -}
 contains :: Int -> [[Int]] -> [[Int]]
-contains n l = filter (elem n) l
+contains n = filter (elem n)
 
 
 {- |
