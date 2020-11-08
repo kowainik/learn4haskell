ce13c37f82d051a5a85c64ca5ac30783b27006ce
said.rahmi <said.rahmi@argus-presse.fr>
2020-11-08 13:10:56 +0100

    Chapter1 solution

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -411,7 +411,7 @@ task is to specify the type of this function.
 >>> squareSum 3 4
 49
 -}
-
+squareSum :: Int -> Int -> Int
 squareSum x y = (x + y) * (x + y)
 
 
@@ -432,7 +432,7 @@ Implement the function that takes an integer value and returns the next 'Int'.
   function body with the proper implementation.
 -}
 next :: Int -> Int
-next x = error "next: not implemented!"
+next x = x + 1
 
 {- |
 After you've implemented the function (or even during the implementation), you
@@ -473,7 +473,8 @@ Implement a function that returns the last digit of a given number.
   whether it works for you!
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Int -> Int
+lastDigit n = n `mod` 10
 
 
 {- |
@@ -503,7 +504,7 @@ branches because it is an expression and it must always return some value.
   satisfying the check will be returned and, therefore, evaluated.
 -}
 closestToZero :: Int -> Int -> Int
-closestToZero x y = error "closestToZero: not implemented!"
+closestToZero x y = if abs x < abs y then x else y
 
 
 {- |
@@ -536,9 +537,14 @@ value after "=" where the condition is true.
 
 Casual reminder about adding top-level type signatures for all functions :)
 -}
-
-mid x y z = error "mid: not implemented!"
-
+mid :: Int -> Int -> Int -> Int
+mid x y z 
+    | x < y && y < z && z > x = y
+    | x < y && y > z && z > x = z
+    | x > y && y < z && z > x = x
+    | x < y && y > z && z < x = x
+    | x < y && y < z && z < x = z
+    | x > y && y > z && z < x = y
 {- |
 =⚔️= Task 8
 
@@ -551,7 +557,10 @@ True
 >>> isVowel 'x'
 False
 -}
-isVowel c = error "isVowel: not implemented!"
+isVowel :: Char -> Bool
+isVowel c 
+    | c `elem` "aeyuioAEYUIO" = True
+    | otherwise = False
 
 
 {- |
@@ -615,7 +624,13 @@ Try to introduce variables in this task (either with let-in or where) to avoid
 specifying complex expressions.
 -}
 
-sumLast2 n = error "sumLast2: Not implemented!"
+
+sumLast2 :: Integral a => a -> a
+sumLast2 n = 
+   let 
+      last2 = n `mod` 100
+   in
+      (last2 `div` 10) + (last2 `mod` 10)
 
 
 {- |
@@ -636,8 +651,12 @@ You need to use recursion in this task. Feel free to return to it later, if you
 aren't ready for this boss yet!
 -}
 
-firstDigit n = error "firstDigit: Not implemented!"
-
+firstDigit :: Integral a => a -> a
+firstDigit n = let 
+                   rest = n `div` 10
+               in
+                 if rest < 10 then rest else firstDigit rest
+    
 
 {-
 You did it! Now it is time to the open pull request with your changes
