828a13b17cd4a4017565689dbe68ed495ad1fcd6
Karun Japhet <karun@japhet.in>
2021-10-08 01:35:55 +0530

    Fix lastDigit implementation for negative numbers

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -492,7 +492,7 @@ Implement a function that returns the last digit of a given number.
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
 lastDigit :: Int -> Int
-lastDigit n = mod n 10
+lastDigit n = mod (abs n) 10
 
 
 {- |
