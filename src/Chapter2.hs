7f7ea4dbf262f1c518a1241e2129b3ffc2ea9b3e
yebowhatsay <mersonmathew@gmail.com>
2022-10-16 22:23:40 +1100

    completed Task 10:Chapter2

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -808,13 +808,15 @@ Let's now try to eta-reduce some of the functions and ensure that we
 mastered the skill of eta-reducing.
 -}
 divideTenBy :: Int -> Int
-divideTenBy x = div 10 x
+divideTenBy = div 10
 
--- TODO: type ;)
-listElementsLessThan x l = filter (< x) l
+
+listElementsLessThan :: Ord a => a -> [a] -> [a]
+listElementsLessThan x = filter (< x)
 
 -- Can you eta-reduce this one???
-pairMul xs ys = zipWith (*) xs ys
+pairMul :: [Integer] -> [Integer] -> [Integer]
+pairMul = zipWith (*)
 
 {- |
 =🛡= Lazy evaluation
