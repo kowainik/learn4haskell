51dfa5228e799886cb41c4b4c668274ec1cdbe03
Jed Aureus Gonzales <gonzales.au@gmail.com>
2022-10-17 21:44:58 +0800

    chore: completed Task 4

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -449,7 +449,7 @@ Implement the function that takes an integer value and returns the next 'Int'.
   function body with the proper implementation.
 -}
 next :: Int -> Int
-next x = error "next: not implemented!"
+next x = x + 1
 
 {- |
 After you've implemented the function (or even during the implementation), you
