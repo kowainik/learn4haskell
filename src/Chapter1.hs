66ae06634b5a813fb2471c899f799c75cfb6241b
Rhythm Bansal <rskbansal@outlook.com>
2022-10-25 15:31:07 +0530

    completed task8

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -570,7 +570,14 @@ True
 >>> isVowel 'x'
 False
 -}
-isVowel c = error "isVowel: not implemented!"
+isVowel :: Char -> Bool
+isVowel c
+    | c == 'a' = True
+    | c == 'e' = True
+    | c == 'i' = True
+    | c == 'o' = True
+    | c == 'u' = True
+    | otherwise = False
 
 
 {- |
