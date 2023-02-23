230e1e37c2339f666ce1c0cbcbf23a1ed18f2d81
Wolfram Sokollek <wolframsokollek@gmail.com>
2023-02-23 22:47:21 +0100

    feat(chapter-1): Task 3  and 4 solved

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -427,7 +427,7 @@ task is to specify the type of this function.
 >>> squareSum 3 4
 49
 -}
-
+squareSum :: Double -> Double -> Double
 squareSum x y = (x + y) * (x + y)
 
 
@@ -448,7 +448,7 @@ Implement the function that takes an integer value and returns the next 'Int'.
   function body with the proper implementation.
 -}
 next :: Int -> Int
-next x = error "next: not implemented!"
+next x = x + 1
 
 {- |
 After you've implemented the function (or even during the implementation), you
