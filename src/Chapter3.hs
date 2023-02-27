d18d537fb651f3606f65c90258b131a05dbb7578
Wolfram Sokollek <wolframsokollek@gmail.com>
2023-02-27 13:55:04 +0100

    feat(chapter-3): Task 1

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -343,6 +343,12 @@ Define the Book product data type. You can take inspiration from our description
 of a book, but you are not limited only by the book properties we described.
 Create your own book type of your dreams!
 -}
+data Book = Book
+    { bookName :: String
+    , bookAuthor :: String
+    , bookPages :: Int
+    , bookCover :: String
+    } deriving (Show)
 
 {- |
 =⚔️= Task 2
